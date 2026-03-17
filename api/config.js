145d65b9e89c5806937e4c0951094a4846968c89
nozutax <nozu.tax@gmail.com>
2026-03-17 19:48:01 +0900

    1947

---
+++ api/config.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+
+  res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL || '',
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || '',
+  });
+}
